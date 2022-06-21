6889d3b0b2cc762ee2bf9705ce81fa106d43b06e
Gaganpreet Kaur Kalsi <gagansinghkalsi4126@gmail.com>
2022-06-22 01:36:39 +0530

    fixing image

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -11,7 +11,7 @@ const links = [
 const SideNavbar = () => {
   return (
     <div className="side-navbar">
-      <img src={"/logo.png"} alt="logo" className="app-logo" />
+      <img src={"./logo.png"} alt="logo" className="app-logo" />
       <hr className="h-line" />
       <div className="nav-links">
         <ul className="link-items">
