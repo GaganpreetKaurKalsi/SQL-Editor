359d062f8bd79323c6ae8cded93323113507bfa1
Gaganpreet Kaur Kalsi <gagansinghkalsi4126@gmail.com>
2022-06-23 03:16:01 +0530

    code splitting done

--- src/pages/SQLEditor.js
+++ src/pages/SQLEditor.js
@@ -1,14 +1,61 @@
-import React, { useEffect, useState, memo } from "react";
-import Editor from "../components/Editor";
-import ResultSection from "../components/ResultSection";
+import React, { useEffect, useState, memo, lazy, Suspense } from "react";
 import SideNavbar from "../components/SideNavbar";
 import { fetchData } from "../helpers/helpers";
 import "./SQLEditor.scss";
 import toast from "react-hot-toast";
 import { Toaster } from "react-hot-toast";
-import SearchComponent from "../components/SearchComponent";
+import { Oval } from "react-loader-spinner";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import TABLE_NAMES from "../constants/constants";
 
+const Editor = lazy(() => import("../components/Editor"));
+
+const SearchComponent = lazy(() => import("../components/SearchComponent"));
+
+const ResultSection = lazy(() => import("../components/ResultSection"));
+
+const skeleton = () => {
+  return (
+    <div className="suspense-skeleton">
+      <div style={{ marginBottom: "10px" }}>
+        <Skeleton count={1} height={30} width={120} />
+      </div>
+      <div style={{ marginBottom: "20px" }}>
+        <Skeleton count={1} height={35} />
+      </div>
+      <div>
+        <Skeleton count={6} height={25} marginBottom={5} />
+      </div>
+    </div>
+  );
+};
+
+const skeletonTable = () => {
+  return (
+    <div className="suspense-skeleton">
+      <div style={{ marginBottom: "30px" }}>
+        <Skeleton count={1} height={30} width={120} />
+      </div>
+      <div>
+        <Skeleton count={8} height={25} marginBottom={5} />
+      </div>
+    </div>
+  );
+};
+
+const loader = () => {
+  return (
+    <Oval
+      ariaLabel="loading-indicator"
+      height={50}
+      width={50}
+      strokeWidth={5}
+      color="#0ea5e9"
+      secondaryColor="#d1d5db"
+    />
+  );
+};
 const SQLEditor = () => {
   const [tableName, setTableName] = useState("Customers");
   const [query, setQuery] = useState("SELECT * FROM Customers");
@@ -27,7 +74,6 @@ const SQLEditor = () => {
   };
 
   const executeQuery = (query) => {
-    console.log("FROM EXECUTE QUERY : ", query);
     if (query === "") {
       toast.custom(
         <div className="customToast">Please enter a query to execute</div>
@@ -68,36 +114,46 @@ const SQLEditor = () => {
       <div className="container">
         <div className="col1">
           <div className="editor-section section">
-            <Editor
-              setQuery={setQuery}
-              query={query}
-              executeQuery={executeQuery}
-            />
+            <Suspense
+              fallback={<div className="suspense-loader">{loader()}</div>}
+            >
+              <Editor
+                setQuery={setQuery}
+                query={query}
+                executeQuery={executeQuery}
+              />
+            </Suspense>
           </div>
           <div className="result-section section">
-            <ResultSection
-              tableName={tableName}
-              result={result}
-              resultIsLoading={resultIsLoading}
-            />
+            <Suspense
+              fallback={<div className="skeleton">{skeletonTable()}</div>}
+            >
+              <ResultSection
+                tableName={tableName}
+                result={result}
+                resultIsLoading={resultIsLoading}
+              />
+            </Suspense>
           </div>
         </div>
         <div className="col2">
           <div className="available-tables-section section">
-            {/* <AvailableTables /> */}
-            <SearchComponent
-              searchList={TABLE_NAMES}
-              head={"Available Tables"}
-              type={"table"}
-            />
+            <Suspense fallback={<div className="skeleton">{skeleton()}</div>}>
+              <SearchComponent
+                searchList={TABLE_NAMES}
+                head={"Available Tables"}
+                type={"table"}
+              />
+            </Suspense>
           </div>
           <div className="history-section section">
-            {/* <HistoryTable history={history} /> */}
-            <SearchComponent
-              searchList={history}
-              head={"History"}
-              type={"history"}
-            />
+            <Suspense fallback={<div className="skeleton">{skeleton()}</div>}>
+              <SearchComponent
+                searchList={history}
+                head={"History"}
+                type={"history"}
+              />
+            </Suspense>
           </div>
         </div>
       </div>
